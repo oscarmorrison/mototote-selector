070ce39178b0deb0685fcc3d2ff70a7f83bfcc5f
Oscar Morrison <me@oscarmorrison.com>
2024-10-08 20:50:11 +1100

    update gitignore and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
         react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'index.html', // Source index.html
+                    dest: '.'          // Destination is the root of the 'public' folder
+                },
+                {
+                    src: 'scrape/output/**/*', // Source all files in 'scrape/output' directory
+                    dest: 'data'               // Destination is 'data' in the 'public' folder
+                }
+            ]
+        })
     ],
     build: {
         outDir: 'public', // Set output to 'public'
